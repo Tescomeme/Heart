e7fa8792eafea24dcfeff9aef19ea09bef9706d0
Tescomeme <taslimayomide@gmail.com>
2024-09-19 13:46:33 -0700

    loov

--- asset/java/app.js
+++ asset/java/app.js
@@ -1,12 +1,15 @@
 const grayHeart = document.querySelector(".gray-heart");
 const redHeart = document.querySelector(".red-heart");
+const clickMe = document.querySelector(".click")
 
 grayHeart.addEventListener("click", () => {
   redHeart.classList.add("animation");
   grayHeart.classList.add("fill-color");
+  clickMe.classList.add("click-js")
 });
 
 redHeart.addEventListener("click", () => {
   redHeart.classList.remove("animation");
   grayHeart.classList.remove("fill-color");
+  clickMe.classList.remove("click-js")
 });
